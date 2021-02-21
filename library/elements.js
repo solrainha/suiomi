ae6594a527c5a7a5ca60b20426c0da69261f123f
Sol <74099580+solrainha@users.noreply.github.com>
2021-02-21 16:26:51 +0000

    Delete elements.js

--- library/elements.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-  $("body").append("<nav> <ul id='menu'> <a href='/' class='logo'><img src='//solrainha.github.io/suiomi/images/logo1.png'/></a> <li><a href='/'>Home</a></li><li><a href='/tagged/blog'>Blog</a></li><li><a href='/services'>Services</a></li><li><a href='/portfolio'>Portfolio</a></li><li><a href='/contact'>Contact</a></li></ul></nav>");
-});
