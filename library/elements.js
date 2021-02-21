61b06bfe164a0b7a3ca3fce39b508127f3147c16
Sol <74099580+solrainha@users.noreply.github.com>
2021-02-21 16:04:00 +0000

    Update elements.js

--- library/elements.js
+++ library/elements.js
@@ -1,14 +1,3 @@
 $(document).ready(function() {
-  $("body").before("<div id='footer-wrapper'><div id='footer-container'><div id='footer-row'><div id='footer-col'><div class='logo-details'><div class='logo'><div class='logo-img'><img src='//solrainha.github.io/suiomi/images/logo1.png'/></div><div class='logo-name'>Suiomi</div></div><div class='logo-desc'>Smart design solutions.</div></div></div><div id='footer-col'><div class='footer-links'><div class='footer-links-title'>Useful Information</div><div class='footer-link'><a href='/'>Terms of Use</a></div><div class='footer-link'><a href='/'>Privacy Policy</a></div><div class='footer-link'><a href='/'>Contact</a></div></div></div><div id='footer-col'><div class='support-details'><div class='support-title'>Support</div><div class='support-email'><a href='mailto:suiomi@hotmail.com'>suiomi@hotmail.com</a></div><div class='support-desc'>Join the support server for a faster reply. Commission support only via email.</div><div id='support'><div class='support-container'><div class='support-icon'><img src='//solrainha.github.io/suiomi/images/logo1.png'/></div><div class='support-details'><div class='sp-title'>Support Server</div></div><div class='sp-button'><a href='//discord.gg/4NXCdxbemJ'>Join</a></div></div></div></div></div></div></div></div>");
-
-
-
-
-
-
-  $('aside.sidebar').stick_in_parent({
-    offset_top: $(window).height() - 150 - $('aside.sidebar').outerHeight() + 0,
-    inner_scrolling: true,
-  });
-  $(document.body).trigger("sticky_kit:recalc");
+  $("body").append("<nav> <ul id='menu'> <a href='/' class='logo'><img src='//solrainha.github.io/suiomi/images/logo1.png'/></a> <li><a href='/'>Home</a></li><li><a href='/tagged/blog'>Blog</a></li><li><a href='/services'>Services</a></li><li><a href='/portfolio'>Portfolio</a></li><li><a href='/contact'>Contact</a></li></ul></nav>");
 });
